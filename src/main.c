c6a14f3aa231a4e31dc4413f779ee8954182c626
CheVitel <vitel2019vitel@gmail.com>
2026-03-11 20:09:40 +0000

    add new C program that sort an array by the one's count in binary

--- src/main.c
+++ src/main.c
@@ -1,8 +1,59 @@
-/* main.c */
 #include <stdio.h>
 
-void main (void)
-{
-        printf ("Hello World\n");
-        printf ("Goodbye World\n");
+int count_ones(int num) {
+    int count = 0;
+    while (num > 0) {
+        if (num % 2 == 1) {
+            count++;
+        }
+        num = num / 2;
+    }
+    return count;
+}
+
+void sort_by_ones(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (count_ones(arr[j]) > count_ones(arr[j + 1])) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Введите размер массива n: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Введите %d натуральных чисел:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nИсходный массив:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    printf("Количество единиц в двоичной записи:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d -> %d единиц\n", arr[i], count_ones(arr[i]));
+    }
+
+    sort_by_ones(arr, n);
+
+    printf("\nОтсортированный массив (по возрастанию количества единиц):\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+
+    return 0;
 }
